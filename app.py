a46fd0f12b76a7bd387afe793321de398260bca1
Sukanya992 <sukanya09200206@gmail.com>
2025-03-24 18:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, this is docker k8's demo!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
